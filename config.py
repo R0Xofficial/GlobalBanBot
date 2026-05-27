36f49a2bf815f878472263721a2e8dfa41cc61dd
R0X <patryk.bieganski1003@gmail.com>
2026-05-27 11:20:57 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # --- config.py ---
 TOKEN = "YOUR_BOT_TOKEN"
-DB_NAME = "gban_system.db"
+DB_NAME = "gbanbotdata.db"
 APPEAL_CHAT_USERNAME = "@YourAppealChat"
 OWNER_ID = 123456789  # Master Admin
 LOG_CHANNEL_ID = -100123456789
