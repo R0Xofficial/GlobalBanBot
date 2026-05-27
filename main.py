ddf0e68eb451e84d1412fa4b4bd2f235bd519b44
R0X <patryk.bieganski1003@gmail.com>
2026-05-27 15:12:24 +0200

    Enhance logging configuration in main.py
    Updated logging configuration to include timestamp and set httpx logger level.

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from config import TOKEN, OWNER_ID, LOG_CHAT_ID, APPEAL_CHAT_USERNAME
 import database as db
 import utils
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
 
 # --- USER LOGGER (Zapisywanie osób do bazy) ---
 
